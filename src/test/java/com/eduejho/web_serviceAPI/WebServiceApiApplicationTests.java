eeb8f4c8ec72b08b9c9a5e6f59fb7a55492bfad9
TheVytch <edu.vytch@gmail.com>
2022-03-25 23:06:59 -0300

    first commit

---
+++ src/test/java/com/eduejho/web_serviceAPI/WebServiceApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eduejho.web_serviceAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebServiceApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
